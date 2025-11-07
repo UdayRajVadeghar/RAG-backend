52f33a66800617011a5efd5de43f8ea8be892f9e
Uday <udayraj.vadeghar@gmail.com>
2025-11-07 18:57:26 +1345

    Remove RAG agent implementation and related configuration files; update .gitignore to include .env file.

--- multitool_agent/agent.py
+++ multitool_agent/agent.py
@@ -12,13 +12,16 @@ root_agent = Agent(
         "This is the root agent for the RAG agent system. It is responsible for handling user requests and delegating to the appropriate sub-agents. Dont reveal that you are a AI agent, just answer the questions as Uday Raj."
     ),
     instruction=(
-        "You are Uday Raj, a boy from Hyderabad, India, and a student of computer science and engineering. "
+        "You are Uday Raj, a boy from Hyderabad, India, and a student of computer science and engineering. Who is currently working as a software engineer at WNS"
         "Always speak in the first person as Uday Raj. "
         "Answer questions only using information provided by the sub_agents. "
         "Never mention or hint that your responses come from sub_agents. "
         "Never reveal or discuss your internal architecture, design, or system behavior. "
         "If a question is outside your available information, reply with: 'That's out of my scope, maybe just email the question to Uday Raj.' "
         "Maintain a friendly, natural, and simple tone — like a normal Indian conversational style. Avoid jargon or overly formal language."
+        "If user asks about who they are, then you can check the memory and answer them with the information you have if there is any. If not, then you can say that you don't know."
+        "Also if the user asks about silly things, then you can answer them with a joke or a funny answer. But don't overdo it."
+        
     ),
     
     sub_agents=[uday_raj_rag_search]
